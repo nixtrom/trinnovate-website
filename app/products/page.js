1f74ee48b2e73172998faed609e4768e2ee27cff
nixtrom <niket.chandil@gmail.com>
2025-04-25 16:40:11 +0530

    Fixed Link issues for illustrations

--- app/products/page.js
+++ app/products/page.js
@@ -1,11 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Products() {
   return <div className="h-screen w-screen flex flex-col items-center justify-center">
    <img src="/images/coding-illustration.svg" className="mb-4"></img>
     <p className="text-xl md:text-3xl mb-4 font-medium text-center">We are revamping ,  Page under Development</p>
-    <p className="text-md md:text-2xl mb-4">Meanwhile have a look at our <a href="/" className="text-red-500 underline cursor-pointer">home page</a> </p>
+    <p className="text-base md:text-2xl mb-4">
+  Meanwhile have a look at our{" "}
+  <Link href="/">
+    <span className="text-red-500 underline cursor-pointer">home page</span>
+  </Link>
+</p>
   </div>
 }
 
